e33e238d4233f0a11677c2f1d4ee500d96cb4e30
Travis J Parker <travis.parker@gmail.com>
2011-02-12 08:28:47 -0800

    it didn't
    Revert "experimenting to see if this fixes an import-time crash on FreeBSD"
    
    This reverts commit c3278dd86f15daec00fde7af75638bec7fac5626.

--- btree.c
+++ btree.c
@@ -1757,7 +1757,7 @@ static PyTypeObject sorted_btree_iterator_type = {
     0,                                              /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE |      /* tp_flags */
         Py_TPFLAGS_HAVE_GC,
-    NULL,                                           /* tp_doc */
+    0,                                              /* tp_doc */
     0,                                              /* tp_traverse */
     0,                                              /* tp_clear */
     0,                                              /* tp_richcompare */
